f52722abeae032e2886751167c7dd4e540fa62d2
Axel <aherreram22@miumg.edu.com>
2023-05-23 21:19:59 -0600

    Modificaciones al entorno

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// function iniciarMap() {
+//   let coord = { lat: 14.851134, lng: -90.070745 };
+//   let map = new google.maps.Map(document.getElementById("sec-maps"), {
+//     zoom: 30,
+//     center: coord,
+//   });
+
+//   let marker  = new google.maps.marker({
+//     position: coord,
+//     map: map
+//   }) 
+// }
